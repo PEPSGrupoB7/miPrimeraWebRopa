5a60f7d565ee521f0a1f942281d95c9ec4a7f633
PEPSGrupoB7 <ciber2@max12>
2026-03-05 14:07:08 +0100

    Actualizar test Puppeteer para apuntar al servicio Docker

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -1,13 +1,17 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
-  const page = await browser.newPage();
+describe('Pruebas E2E de mi web', () => {
+  test('Página principal carga y título correcto', async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:9081');
 
-  await page.goto('http://localhost:3000');
+    const title = await page.title();
+    expect(title).toBe('Mi Primera Web Ropa');
 
-  const title = await page.title();
-  console.log("Título:", title);
-
-  await browser.close();
-})();
+    await browser.close();
+  });
+});
